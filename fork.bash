8c983efd78eacd575c047d809101d7a948b8e16b
Siphamandla Mjoli <brianm445@yahoo.com>
2024-06-16 22:38:54 +0200

    update: first commit

--- fork.bash
+++
@@ -1,1 +0,0 @@
-anvil  --port 8545 --fork-url "https://mainnet.infura.io/v3/25441638db5d4f86b757bc8be3fcda5f" --fork-block-number '20093564'  --fork-chain-id 1  --no-rate-limit --mnemonic  "claw aware right regular security second any useless turkey agent eagle announce"   --retries 1000000000 
